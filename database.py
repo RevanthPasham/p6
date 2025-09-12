20533f6b4b8ded152d13ce22d1f11c777d35fd0a
pasham Revanth <pashamrevanth541@gmail.com>
2025-09-13 00:26:27 +0530

    changing database file

--- database.py
+++ database.py
@@ -7,12 +7,17 @@ load_dotenv()  # loads values from .env
 db_url = os.environ.get("DATABASE_URL")
 ca_cert_path = os.environ.get("CA_CERT_PATH")
 
+if not db_url:
+    raise ValueError("DATABASE_URL environment variable is not set. Please set it in your environment or .env file.")
 
-# Create SQLAlchemy engine with SSL
-engine = create_engine(
-    db_url,
-    connect_args={"ssl_ca": ca_cert_path}
-)
+# Create SQLAlchemy engine with SSL if ca_cert_path is set
+if ca_cert_path:
+    engine = create_engine(
+        db_url,
+        connect_args={"sslrootcert": ca_cert_path}
+    )
+else:
+    engine = create_engine(db_url)
 
 def load_jobs_from_db():
     with engine.connect() as conn:
